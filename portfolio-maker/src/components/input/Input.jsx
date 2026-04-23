927237a93612b5fede9581d609463463ab6e6a52
Aditya Kumar <adityaforwork101@gmail.com>
2026-04-23 15:22:44 +0530

    input section done

--- portfolio-maker/src/components/input/Input.jsx
+++ portfolio-maker/src/components/input/Input.jsx
@@ -1,34 +1,64 @@
-"use client"
-import React, { useState } from 'react'
+"use client";
+import React, { useState } from "react";
 
-function Input({data, setData}) {
+function Input({ data, setData }) {
   const [skillInput, setSkillInput] = useState("");
 
-   // 🔹 Handle simple fields
+  // 🔹 Handle simple fields
   const handleChange = (field, value) => {
     setData({ ...data, [field]: value });
   };
 
   // 🔹 Skills handlers
-  const addSkill = () => {
-    if(!skillInput.trim()) return;
+  const addSkill = (e) => {
+    e.preventDefault();
+    if (!skillInput.trim()) return;
 
     setData({
       ...data,
-      skills : [...data.skills, skillInput]
+      skills: [...data.skills, skillInput],
     });
     setSkillInput("");
-  }
+  };
 
   const removeSkill = (i) => {
     const updated = data.skills.filter((_, index) => index !== i);
     setData({ ...data, skills: updated });
+  };
+
+  // 🔹 Projects handlers
+
+  const addProject = () => {
+    setData({
+      ...data,
+      projects: [...data.projects, { title: "", desc: "", link: "" }],
+    });
+  };
+
+  const updateProject = (index, field, value) => {
+    const updated = [...data.projects];
+    updated[index][field] = value;
+
+    setData({ ...data, projects: updated });
+  };
+
+  const removeProject = (index) => {
+    const updated = data.projects.filter((_, i) => index !== i )
+    setData({...data, projects: updated});
   }
 
-  return (
-    <div className="w-full md:w-1/2 space-y-6 overflow-y-auto max-h-[90vh]">
+  // Social Handlers
 
-      <p className='text-2xl text-[#90A955] font-semibold'>Fill Following Details:</p>
+  const handleSocial = (field, value) => {
+    setData({...data, social : {...data.social, [field] : value}})
+  }
+
+
+  return (
+    <div className="w-full md:w-1/2 space-y-6 overflow-y-auto min-h-[90vh]">
+      <p className="text-2xl text-[#90A955] font-semibold">
+        Fill Following Details:
+      </p>
 
       {/* Personal Info */}
       <div className="border p-4 rounded-xl bg-[#4F772D] border-[#132A13]">
@@ -52,16 +82,21 @@ function Input({data, setData}) {
       {/* Skills */}
       <div className="border p-4 rounded-xl bg-[#4F772D] border-[#132A13]">
         <h2 className="font-bold mb-2 text-black">Skills</h2>
-         <div className="flex gap-2">
-          <input
-            className="border py-2 px-4 flex-1 rounded-lg focus:outline-none focus:ring-2 focus:ring-[#90A955]"
-            placeholder="Add skill"
-            value={skillInput}
-            onChange={(e) => setSkillInput(e.target.value)}
-          />
-          <button onClick={addSkill} className="px-3 bg-[#31572C] rounded-lg text-white hover:bg-[#132A13]">
-            Add
-          </button>
+        <div>
+          <form onSubmit={(e) => addSkill(e)} className="flex gap-2">
+            <input
+              className="border py-2 px-4 flex-1 rounded-lg focus:outline-none focus:ring-2 focus:ring-[#90A955]"
+              placeholder="Add skill"
+              value={skillInput}
+              onChange={(e) => setSkillInput(e.target.value)}
+            />
+            <button
+              type="submit"
+              className="px-3 bg-[#31572C] rounded-lg text-white hover:bg-[#132A13]"
+            >
+              Add
+            </button>
+          </form>
         </div>
 
         <div className="flex flex-wrap gap-2 mt-3">
@@ -77,9 +112,63 @@ function Input({data, setData}) {
         </div>
       </div>
 
-     
+      {/* Projects */}
+
+      <div className="border p-4 rounded-xl bg-[#4F772D] border-[#132A13]">
+        <h2 className="font-bold mb-2 text-black">Projects</h2>
+        {data.projects.map((project, index) => (
+          <div
+            key={index}
+            className="border-3 border-black p-4 rounded-xl mb-4 flex flex-col gap-3"
+          >
+            <input
+              placeholder="title"
+              value={project.title}
+              onChange={(e) => updateProject(index, "title", e.target.value)}
+              className="border py-2 px-4 flex-1 rounded-lg focus:outline-none focus:ring-2 focus:ring-[#90A955]"
+            />
+            <input
+              placeholder="description"
+              value={project.description}
+              onChange={(e) =>
+                updateProject(index, "description", e.target.value)
+              }
+              className="border py-2 px-4 flex-1 rounded-lg focus:outline-none focus:ring-2 focus:ring-[#90A955]"
+            />
+            <input
+              placeholder="link"
+              value={project.link}
+              onChange={(e) => updateProject(index, "link", e.target.value)}
+              className="border py-2 px-4 flex-1 rounded-lg focus:outline-none focus:ring-2 focus:ring-[#90A955]"
+            />
+            <button
+              onClick={() => removeProject(index)}
+              className="bg-red-600 text-white px-3 py-1 rounded-lg hover:bg-red-800 self-end"
+            >
+              Remove Project
+            </button>
+          </div>
+        ))}
+        <button
+          className="px-4 py-2 bg-[#31572C] rounded-lg text-white hover:bg-[#132A13]"
+          onClick={addProject}
+        >
+          Add Project
+        </button>
+      </div>
+
+      {/* Social Links */}
+
+      <div className="border p-4 rounded-xl bg-[#4F772D] border-[#132A13]">
+        <h2 className="font-bold mb-2 text-black">Social Links</h2>
+        <div className="flex flex-col gap-3">
+        <input type="text" placeholder="GitHub" value={data.social.github} onChange={e => handleSocial("github", e.target.value)} className="border py-2 px-4 flex-1 rounded-lg focus:outline-none focus:ring-2 focus:ring-[#90A955]"/>
+        <input type="text" placeholder="LinkedIn" value={data.social.linkedin} onChange={e => handleSocial("linkedin", e.target.value)} className="border py-2 px-4 flex-1 rounded-lg focus:outline-none focus:ring-2 focus:ring-[#90A955]"/>
+        </div>
+      </div>
+
     </div>
   );
 }
 
-export default Input
+export default Input;
