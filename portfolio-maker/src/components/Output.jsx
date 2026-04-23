e8504f961bcd328f52c1fdb5019c9ca0fb5a099a
Aditya Kumar <adityaforwork101@gmail.com>
2026-04-23 15:32:52 +0530

    output skeleton

--- portfolio-maker/src/components/Output.jsx
+++ portfolio-maker/src/components/Output.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 
-function Output() {
+function Output({data}) {
   return (
-    <div>
-      Output here
+    <div className="w-full md:w-1/2 space-y-6 overflow-y-auto min-h-[90vh]">
+            <>
+            <p>{data.name}</p>
+            <p>{data.bio}</p>
+            <p>{data.skills.join(", ")}</p>
+            <p>{data.projects.map((project, i) => <span key={i}>{project.title}, </span>)}</p>
+            <p>{data.social.github}</p>
+            <p>{data.social.linkedin}</p>
+            </>
+      
     </div>
   )
 }
